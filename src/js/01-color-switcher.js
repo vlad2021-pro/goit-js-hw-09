a8d6de3210871b237137c46d3d34b4f1f152bc3f
vlad2021-pro <84739712+vlad2021-pro@users.noreply.github.com>
2021-11-15 17:01:43 +0300

    22

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
 
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+let timerId = null
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+const onSwitchTheme = e => {
+  e.target.dissabled = true
+  timerId = setInterval(() => {refs.bodyEl.style.backgroundColor = getRandomHexColor()  }, 1000)
+}
+// console.log(e.target)
+// const onSwitchTheme = e => { }
+
+
+refs.startBtn.addEventListener('click', onSwitchTheme)
+// refs.stopBtn.addEventListener('click', offSwitchTheme)
